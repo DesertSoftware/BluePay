98ae175af8ceef54c0b119fa930e2f07d893d54c
rostreim <ron@desertsoftware>
2013-04-18 09:03:34 -0700

    Updated nuspec and assembly info

--- src/DesertSoftware.BluePay/Properties/AssemblyInfo.cs
+++ src/DesertSoftware.BluePay/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DesertSoftware.BluePay")]
-[assembly: AssemblyDescription("BluePay Gateway payment processor interface")]
+[assembly: AssemblyTitle("DesertSoftware BluePay")]
+[assembly: AssemblyDescription("A .net payment processor interface to BluePay Gateway")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DesertSoftware")]
 [assembly: AssemblyProduct("DesertSoftware.BluePay")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
